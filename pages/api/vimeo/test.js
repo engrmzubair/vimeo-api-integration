70bf33d02c88a57c309fcc5efa4d70047a2bd3a1
Engr. Muhammad Zubair <engrmzubair2@gmail.com>
2022-08-01 13:44:57 +0500

    vimeo api configuration done

--- pages/api/vimeo/test.js
+++ pages/api/vimeo/test.js
@@ -1,13 +1,22 @@
 let Vimeo = require('vimeo').Vimeo;
-let client = new Vimeo("{client_id}", "{client_secret}", "{access_token}");
 
-client.request({
-    method: 'GET',
-    path: '/tutorial'
-}, function (error, body, status_code, headers) {
-    if (error) {
-        console.log(error);
-    }
+const clientId = "b60a4020ce29219884d81be8b146229e6e3b5a96",
+    client_secret = "5gi7RwGawiOT//yq0ckbsrjNqYkDQmCpO6WhvLE7SnYPRGlJYj7rKUZGpcUZjyMu3gvq74wFMBDQOw2MSf6alPXFHNqpVQ/APuDyqUBjXttkO4Yn3ybc1neDaBUaMvAI",
+    access_token = "3dd7a5f39b2ea84bbfb5b742fad1dba2";
 
-    console.log(body);
-})
+let client = new Vimeo(clientId, client_secret, access_token);
+
+export default function handler(req, res) {
+    client.request({
+        method: 'GET',
+        path: '/tutorial'
+    }, function (error, body, status_code, headers) {
+        if (error) {
+            console.log(error);
+            res.status(401).json({ error })
+        }
+
+        console.log(body);
+        res.status(200).json({ body })
+    })
+}
